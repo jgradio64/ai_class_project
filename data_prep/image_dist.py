0f459c697ad1a6bb99586a9434914816ee929166
Ben Eich <jgradio64@yahoo.com>
2023-04-06 21:01:45 -0400

    Added functions to copy and resize the images

--- data_prep/image_dist.py
+++ data_prep/image_dist.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from statistics import median
 import matplotlib.pyplot as plt
-import glob, os
+import os, shutil
 
 widths = []
 heights = []
@@ -14,12 +14,16 @@ median_width = None
 
 # image folder is located up one directory
 os.chdir("..")
+# Get the base directory
+root_dir = os.getcwd()
 # change to data folder
 os.chdir('data')
 # Sets the path to the dataset folder
 path = os.getcwd()
 
+
 def get_image_info(path):
+	global big_img_count, little_img_count
 	print("getting image info");
 	#Iterate through all images
 	for root, dirs, files in os.walk(path):
@@ -27,8 +31,14 @@ def get_image_info(path):
 			with Image.open(os.path.join(root, file)) as img:
 				#Record width and height
 				width, height = img.size
-				widths.append(width)
-				heights.append(height)
+				if(width > 2500):
+					continue
+				elif(width < 200):
+					continue
+				else:
+					widths.append(width)
+					heights.append(height)
+	
 
 def calculate_statistics():
 	# #Max, min, and mean of the widths of the images
@@ -42,22 +52,59 @@ def calculate_statistics():
 	min_height = min(heights)
 	median_height = median(heights)
 
+
 def show_statistics():
 	print("minimum width | height \n" + str(min_width) + "\t" + str(min_height))
 	print("Maximum width | height \n" + str(max_width) + "\t" + str(max_height))
 	print("Median width | height \n" + str(median_width) + "\t" + str(median_height))
 
+
 def show_histogram():
 	#Create histogram
-	plt.hist(widths, bins=30, alpha=0.5, label='Width')
-	plt.hist(heights, bins=30, alpha=0.5, label='Height')
+	plt.hist(widths, bins=50, alpha=0.5, label='Width')
+	plt.hist(heights, bins=50, alpha=0.5, label='Height')
 	plt.legend(loc='upper right')
 	plt.title('Distribution of Image Dimensions')
 	plt.xlabel('Number of Pixels')
 	plt.ylabel('Frequency')
 	plt.show()
 
+
+def resize_images(wd, ht):
+	print("initialize folder for formatted data")
+	path = os.path.join(root_dir, "formatted_data")
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			delete_img = False
+			with Image.open(os.path.join(root, file)) as img:
+				#Record width and height
+				width, height = img.size
+				if(width > 2500):
+					delete_img = True
+				elif(width < 200):
+					delete_img = True
+				else:
+					#Resize the image
+					resized_img = img.resize((wd, ht))
+					resized_img.save(os.path.join(root, file))
+			if(delete_img):
+				os.remove(os.path.join(root, file))
+
+
+def copy_images():
+	source_dir = os.path.join(root_dir, "data")
+	destination_dir = os.path.join(root_dir, "formatted_data")
+	# Check if the directory already exists. Cannot copy to a location that already exists
+	if(os.path.isdir(destination_dir)):
+		# Delete the folder
+		shutil.rmtree(destination_dir) 
+	
+	shutil.copytree(source_dir, destination_dir)
+
+
 get_image_info(path)
 calculate_statistics()
 show_statistics()
 show_histogram()
+copy_images()
+resize_images(int(median_width), int(median_height))
