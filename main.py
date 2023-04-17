933562904a9fe7de5bd000df5d9969c2f1b7851c
Ben Eich <jgradio64@yahoo.com>
2023-04-17 18:04:57 -0400

    attempt at implementing resnet50

--- main.py
+++ main.py
@@ -1,1 +1,106 @@
-# This will be the main file for the CNN to train.
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.models import Sequential
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+import os
+
+
+# Get image size from the images themselves
+# Go to fmt_data folder, and pick an image.
+
+IMAGE_SIZE = [640, 426]
+
+# Get the base directory
+root_dir = os.getcwd()
+
+# Relative path to training and testing folders.
+train_Path = root_dir + "/formatted_data/train"
+test_Path = root_dir + "/formatted_data/test"
+
+resnet = ResNet50(
+    input_shape = IMAGE_SIZE + [3], # Making the image into 3 Channel, so concating 3.
+    weights = 'imagenet', # Default weights.
+    include_top = False   # 
+)
+
+# resnet.summary()
+
+for layer in resnet.layers:
+    layer.trainable = False
+
+folders = glob(train_Path + '/*')
+# print(folders)
+
+# Set the flatten layer.
+vehicle_label = ['car', 'minivan', 'suv', 'truck', 'van']
+
+x = Flatten() (resnet.output)
+
+prediction = Dense(len(folders), activation = 'softmax')(x)
+
+# Create a model Object
+model = Model(inputs = resnet.input, outputs = prediction)
+# model.summary()
+
+model.compile (
+    loss = 'categorical_crossentropy',
+    optimizer = 'adam',
+    metrics = ['accuracy']
+)
+
+# Use the Image Data Generator
+# from tensorflow.keras.proprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+    rescale = 1./255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True
+)
+
+test_datagen = ImageDataGenerator(
+    rescale = 1./255
+)
+
+training_set = train_datagen.flow_from_directory(
+    train_Path,
+    target_size = IMAGE_SIZE,
+    batch_size = 32,
+    class_mode = 'categorical' # As we have more than 2 so using categorical.. for 2 we might have used binary.
+)
+
+test_set = train_datagen.flow_from_directory(
+    test_Path,
+    target_size = IMAGE_SIZE,
+    batch_size = 32,
+    class_mode = 'categorical'
+)
+
+# Fit the model.
+
+history = model.fit_generator(
+    training_set,
+    validation_data = test_set,
+    epochs = 50,
+    steps_per_epoch = len(training_set),
+    validation_steps = len(test_set)
+)
+
+# Plot the Loss
+plt.plot(history.history['loss'], label = 'train_loss')
+plt.plot(history.history['val_loss'], label ='val loss')
+plt.legend()
+plt.show()
+# plt.savefig('LossVal_loss')
+
+# Plot the Accuracy
+plt.plot(history.history['accuracy'], label = 'train accuracy')
+plt.plot(history.history['val_accuracy'], label ='val accuracy')
+plt.legend()
+plt.show()
+# plt.savefig('valAccuracy')
